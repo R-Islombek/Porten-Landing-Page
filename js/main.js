363e3c5172ae0111dad81eaec9310f866f422b35
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-10 04:26:48 +0500

    header va hero Responsivelik qo'shdim

--- js/main.js
+++ js/main.js
@@ -28,3 +28,44 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+
+// 1-Funcksiyalar masalalar ishlash;
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+
+// function sum(a, b){
+//     S = a * b;
+//     P = 2 * (a + b);
+//     console.log(`S = ${S}, P = ${P}`);
+// }
+// sum(a, b)
+
+// 2-Masala;
+
+// let a = prompt("a sonini kiriting");
+
+// function sum(a){
+//     S = Math.pow(a, 2);
+//     P = 4 * a;
+//     console.log(`S = ${S}, P = ${P}`);
+// }
+// sum(a)
+
+// 3-Masala;
+
+// let a = Number(prompt("a sonini kiriting"));
+
+// function sum(a){
+//     sum = 0;
+
+//     for (let i = 0; i < a; i++) {
+//         sum += i;
+//     }
+//  console.log(sum);
+ 
+// }
+// sum(a)
